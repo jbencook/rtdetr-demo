7181c3187b089700f658dbc51cf2ce484d929269
jbencook <ben@sparrow.dev>
2024-04-10 21:22:41 +0000

    evaluate checkpoint path

--- retinanet_demo/evaluate.py
+++ retinanet_demo/evaluate.py
@@ -1,14 +1,14 @@
 """Evaluate the model."""
-import pytorch_lightning as pl
-import torch
+from pathlib import Path
+from typing import Union
 
-from .config import Config
+import pytorch_lightning as pl
+
 from .train import Lightning
 
 
-def evaluate_model() -> None:
+def evaluate_model(checkpoint_path: Union[str, Path]) -> None:
     """Evaluate the model."""
-    lightning = Lightning()
-    lightning.model.load_state_dict(torch.load(Config.model_path))
+    lightning = Lightning.load_from_checkpoint(checkpoint_path)
     trainer = pl.Trainer(accelerator="gpu", devices=1)
     trainer.test(lightning)
